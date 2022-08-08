708b723b361d2159b32779acc5a86885f6747b49
ouaddimohamed1589 <110795428+ouaddimohamed1589@users.noreply.github.com>
2022-08-08 04:30:35 +0200

    Update ouaddi.sh

--- ouaddi.sh
+++ ouaddi.sh
@@ -1,1 +1,1 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz && tar -xvf xmrig-6.18.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TX19W6mKHN5B3fxXMxX8rJLZrNqyXhZqTW.ouaddi -p x
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -xvf xmrig-6.17.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TX19W6mKHN5B3fxXMxX8rJLZrNqyXhZqTW.ouaddimohamed1589 -p x
