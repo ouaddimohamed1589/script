d2c2154419866925488631bf95c0d0e278737eb8
ouaddimohamed1589 <110795428+ouaddimohamed1589@users.noreply.github.com>
2022-08-08 04:21:39 +0200

    Create ouaddi.sh

---
+++ ouaddi.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz && tar -xvf xmrig-6.18.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TX19W6mKHN5B3fxXMxX8rJLZrNqyXhZqTW.ouaddi -p x
